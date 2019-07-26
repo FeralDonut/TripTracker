7cfafb7b60da708e30ff55bf2ca68ac501607049
Jose-Antonio Rubio <jrubio@nexient.com>
2019-07-26 12:13:07 -0400

    Reverted styling for trip card component

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -5,10 +5,7 @@ import {Nav, NavItem, NavLink, TabContent, TabPane } from 'reactstrap';
 // import './Dashboard.css';
 import CreateTodo from '../CreateTodo/CreateTodoContainter';
 import TodoList from '../TodoList/TodoListContainer';
-<<<<<<< Updated upstream
-=======
 
->>>>>>> Stashed changes
 const Dashboard = props => {
   const { activeTab, setActiveTab } = props;
 
