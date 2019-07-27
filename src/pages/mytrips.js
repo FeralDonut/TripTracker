04b5d6045d969f56313ebf28257eff99c6678d93
Jose-Antonio Rubio <jrubio@nexient.com>
2019-07-27 13:42:56 -0400

    added link to show api call for midtermDemo

--- src/pages/mytrips.js
+++ src/pages/mytrips.js
@@ -5,7 +5,11 @@ import SEO from "../components/seo"
 const SecondPage = () => (
   <Layout>
     <SEO title="My Trips" />
-    <h1>Coming Soon!</h1>
+    <h3>Demo of a GET call to our back end</h3>
+    <a href="http://24.4.98.147:8000/api/trips/5d38c0ea13132b226ebebf25" target="_blank">
+      Click Me
+    </a>
+    
   </Layout>
 )
 
