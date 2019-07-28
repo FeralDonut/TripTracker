dea5e012d2d4429a498bcff6cd921ade03a65dc9
Jose-Antonio Rubio <jrubio@nexient.com>
2019-07-28 14:00:56 -0400

    Capturing checkbox

--- src/components/TodoList/TodoList.js
+++ src/components/TodoList/TodoList.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
@@ -7,26 +7,34 @@ import Checkbox from '@material-ui/core/Checkbox';
 import IconButton from '@material-ui/core/IconButton';
 import DeleteIcon from '@material-ui/icons/Delete';
 
-const TodoList = ({ todos, deleteTodo }) => (
-  <List>
-    {todos.map(todo => (
-      <ListItem key={todo.id} dense button>
-        <Checkbox tabIndex={-1} disableRipple />
-        <ListItemText primary={todo.description} />
-        <ListItemSecondaryAction>
-          <IconButton
-            aria-label="Delete"
-            onClick={() => {
-              deleteTodo(todo.id);
-            }}
-          >
-            <DeleteIcon />
-          </IconButton>
-        </ListItemSecondaryAction>
-      </ListItem>
-    ))}
-  </List>
-);
+const TodoList = ({ todos, deleteTodo }) => {
+  const [ checked, setChecked ] = useState(true);
+
+  const handleClick = () => {
+    setChecked(!checked);
+    console.log("Checkbox is checked = ", checked)
+  }
+
+  return (
+    <List>
+      {todos.map(todo => (
+        <ListItem key={todo.id} dense button>
+          <Checkbox tabIndex={-1} disableRipple onClick={() => handleClick()}/>
+          <ListItemText primary={todo.description} />
+          <ListItemSecondaryAction>
+            <IconButton
+              aria-label="Delete"
+              onClick={() => {
+                deleteTodo(todo.id);
+              }}
+            >
+              <DeleteIcon />
+            </IconButton>
+          </ListItemSecondaryAction>
+        </ListItem>
+      ))}
+    </List>
+)};
 
 export default TodoList;
 
