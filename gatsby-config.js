8daa399a748ddf8821b0dadd96d0bb13abc92536
Jose-Antonio Rubio <jrubio@nexient.com>
2019-08-08 12:06:49 -0400

    Updated SEO and description

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,19 +1,19 @@
 module.exports = {
   siteMetadata: {
     title: `TripTracker`,
-    description: `PlaceHolder`,
-    author: `@gatsbyjs`,
+    description: `Start planning your next trip!`,
+    author: `@gatsbyjs`
   },
   plugins: [
     {
-      resolve: `gatsby-plugin-react-helmet`,
+      resolve: `gatsby-plugin-react-helmet`
     },
     {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `images`,
-        path: `${__dirname}/src/images`,
-      },
+        path: `${__dirname}/src/images`
+      }
     },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-styled-components`,
@@ -27,11 +27,11 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
-      },
-    },
+        icon: `src/images/gatsby-icon.png` // This path is relative to the root of the site.
+      }
+    }
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.app/offline
     // 'gatsby-plugin-offline',
-  ],
-}
+  ]
+};
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,53 +1,53 @@
-import React, {useState, useEffect} from "react"
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Layout from "../components/layout"
-import SEO from "../components/seo"
-import { Jumbotron } from 'reactstrap';
-import style from './styles.module.css'
-import user from '../utils/mockData';
-import TripList from '../components/Trips/TripList';
-import { library } from '@fortawesome/fontawesome-svg-core';
-import { faUserAstronaut } from '@fortawesome/free-solid-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import React, { useState, useEffect } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Layout from "../components/layout";
+import SEO from "../components/seo";
+import { Jumbotron } from "reactstrap";
+import style from "./styles.module.css";
+import user from "../utils/mockData";
+import TripList from "../components/Trips/TripList";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faUserAstronaut } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
-library.add(faUserAstronaut)
+library.add(faUserAstronaut);
 
 const IndexPage = () => {
-  const [trips, setTrips] = useState ([])
-  useEffect(()=> {
+  const [trips, setTrips] = useState([]);
+  useEffect(() => {
     fetch(`http://24.4.98.147:8000/api/trips/`)
-    .then(res => res.json())
-    .then(data => setTrips(data))
-  }, [])
+      .then(res => res.json())
+      .then(data => setTrips(data));
+  }, []);
 
   return (
     <Layout>
-      <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-      <div
-        className={style.container}
-        id="MyApp"
-      >
-      <Jumbotron className={style.jumbo_container}>
+      <SEO title="TripTracker" keywords={[`gatsby`, `application`, `react`]} />
+      <div className={style.container} id="MyApp">
+        <Jumbotron className={style.jumbo_container}>
           <div className={style.welcome_container}>
             <div className={style.greeting}>
               <h1>TripTracker</h1>
               <h4 className="welcome">Welcome {user.displayName}!</h4>
             </div>
-            <div >
-              <FontAwesomeIcon icon={faUserAstronaut} size="7x" className={style.icon}/>
+            <div>
+              <FontAwesomeIcon
+                icon={faUserAstronaut}
+                size="7x"
+                className={style.icon}
+              />
             </div>
           </div>
-            <p>Use the navigation bar at the top of the page to get started.</p>
+          <p>Use the navigation bar at the top of the page to get started.</p>
         </Jumbotron>
-        <TripList trips={trips}/>
+        <TripList trips={trips} />
       </div>
     </Layout>
-  )
-}
+  );
+};
 
 export default IndexPage;
 // Connect the page to the store
 // export default connect(state => ({
 //   isDarkMode: state.app.isDarkMode
 // }), null)(IndexPage)
-
